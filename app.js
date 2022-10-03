1132be62fa6475e00ee9183ccdacacdb8d2cc7dd
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-10-03 09:48:23 -0400

    express router

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
-var app = express();
-
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
+
+var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -25,7 +25,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);

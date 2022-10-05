74590e75c6684ed7ae98b0822384480c19883f18
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-10-05 16:58:42 -0400

    update router

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,51 +1,83 @@
 const express = require("express");
+const Campsite = require("../models/campsite");
+
 const campsiteRouter = express.Router();
 
 campsiteRouter
   .route("/")
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    next(); //first method that filters through and then routes to other methods. Prepackaged response to client
-  }) //next() means "proceed to next middleware"
-  .get((req, res) => {
-    res.end("Will send all the campsites to you"); //res.end()ends the response (or res.json() if using json)
+  .get((req, res, next) => {
+    Campsite.find()
+      .then((campsites) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(campsites);
+      })
+      .catch((err) => next(err));
   })
-  .post((req, res) => {
-    res.end(
-      `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
-    );
+  .post((req, res, next) => {
+    Campsite.create(req.body)
+      .then((campsite) => {
+        console.log("Campsite Created ", campsite);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(campsite);
+      })
+      .catch((err) => next(err));
   })
   .put((req, res) => {
     res.statusCode = 403;
     res.end("PUT operation not supported on /campsites");
   })
-  .delete((req, res) => {
-    res.end("Deleting all campsites");
+  .delete((req, res, next) => {
+    Campsite.deleteMany()
+      .then((response) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(response);
+      })
+      .catch((err) => next(err));
   });
 
-//campsiteId Routes
 campsiteRouter
   .route("/:campsiteId")
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    next();
-  })
-  .get((req, res) => {
-    res.end(`Will send details of the campsite ${req.params.campsiteId}`); //params object is part of req
+  .get((req, res, next) => {
+    Campsite.findById(req.params.campsiteId)
+      .then((campsite) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(campsite);
+      })
+      .catch((err) => next(err));
   })
   .post((req, res) => {
     res.statusCode = 403;
-    res.end("POST request not supported");
-  })
-  .put((req, res) => {
     res.end(
-      `Will update campsite: ${req.body.name} and description: ${req.body.description}`
+      `POST operation not supported on /campsites/${req.params.campsiteId}`
     );
   })
-  .delete((req, res) => {
-    res.end(`Deleting campsite with id: ${req.params.id}`);
+  .put((req, res, next) => {
+    Campsite.findByIdAndUpdate(
+      req.params.campsiteId,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    )
+      .then((campsite) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(campsite);
+      })
+      .catch((err) => next(err));
+  })
+  .delete((req, res, next) => {
+    Campsite.findByIdAndDelete(req.params.campsiteId)
+      .then((response) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(response);
+      })
+      .catch((err) => next(err));
   });
 
 module.exports = campsiteRouter;

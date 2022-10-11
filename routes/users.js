f390c03c198fb92fd0be3bbc74634b4caca0860e
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-10-11 14:43:40 -0400

    passport

--- routes/users.js
+++ routes/users.js
@@ -1,50 +1,39 @@
 var express = require("express");
 const User = require("../models/user");
 const passport = require("passport");
+const { response } = require("express");
 
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res) {});
+router.get("/", function (req, res) {
+  res.send("respond with a resource");
+});
 
-router.post("/login", (req, res, next) => {
-  if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      const err = new Error("You are not authenticated");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
-    const auth = Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    const username = auth[0];
-    const password = auth[1];
-
-    User.findOne({ username: username })
-      .then((user) => {
-        if (!user) {
-          const err = new Error(`User ${username} does not exist`);
-          err.status = 401;
-          return next(err);
-        } else if (user.password !== password) {
-          const err = new Error("Your password is incorrect");
-          err.status = 401;
-          return next(err);
-        } else if (user.username === username && user.password === password) {
-          req.session.user = "authenticated";
+router.post("/signup", (req, res, next) => {
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    (err) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "application.json");
+        res.json({ err: err });
+      } else {
+        passport.authenticate("local")(req, res, () => {
           res.statusCode = 200;
-          res.setHeader("Content-Type", "text/plain");
-          res.end("You are authenticated");
-        }
-      })
-      .catch((err) => next(err));
-  } else {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("You are already authenticated");
-  }
+          res.setHeader("Content-Type", "application/json");
+          res.json({ success: true, status: "Registration Successful" });
+        });
+      }
+    }
+  );
+});
+
+router.post("/login", passport.authenticate("local"), (req, res) => {
+  response.statusCode = 200;
+  setHeader("Content-Type", "application/json");
+  res.json({ success: true, status: "You have successfully logged in!" });
 });
 
 router.get("/logout", (req, res, next) => {

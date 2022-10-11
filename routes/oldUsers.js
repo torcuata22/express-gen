9cabecd6d02e39014dc3ea7609b2498a5c2f4a1f
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-10-11 14:33:08 -0400

    serializers

--- routes/oldUsers.js
+++ routes/oldUsers.js
@@ -5,7 +5,32 @@ const passport = require("passport");
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res) {});
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/signup", (req, res, next) => {
+  User.findOne({ username: req.body.username })
+    .then((user) => {
+      if (user) {
+        const err = new Error(`User ${req.body.username} already exists!`);
+        err.status = 403;
+        return next(err);
+      } else {
+        User.create({
+          username: req.body.username,
+          password: req.body.password,
+        })
+          .then((user) => {
+            res.statusCode = 200;
+            res.setHeader("Content-type", "application/json");
+            res.json({ status: "Registration Successful!", user: user });
+          })
+          .catch((err) => next(err));
+      }
+    })
+    .catch((err) => next(err));
+});
 
 router.post("/login", (req, res, next) => {
   if (!req.session.user) {

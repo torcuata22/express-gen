2234f6bc716b4775393466ac7c50cf5ac0cbacb8
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-10-11 16:36:27 -0400

    authenticate dot js

--- authenticate.js
+++ authenticate.js
@@ -8,3 +8,28 @@ const jwt = require("jsonwebtoken");
 exports.local = passport.use(new LocalStrategy(User.authenticate())); //LocalStrategy requires verified callback function
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+exports.getToken = function (user) {
+  return jwt.sign(user, config.secretKey, { expiresIn: 3600 });
+};
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+const jwt = config.secretKey;
+
+exports.jwtPassport = passport.use(
+  new JWTStrategy(opts, (jwt_payload, done) => {
+    console.log("JWT payload: ", jwt_payload);
+    User.findOne({ _id: jwt_payload._id }, (err, user) => {
+      if (err) {
+        return done(err, false);
+      } else if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    });
+  })
+);
+
+exports.verifyUser = passport.authenticate("jwt", { session: false });

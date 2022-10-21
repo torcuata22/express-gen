fdd15cf56d4a150b828dd0d8a46586917fde9d80
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-10-21 14:37:35 -0400

    finished third party auth with fb

--- routes/users.js
+++ routes/users.js
@@ -22,6 +22,23 @@ router.get(
   }
 );
 
+router.get(
+  "/facebook/token",
+  passport.authenticate("facebook-token"),
+  (req, res) => {
+    if (req.user) {
+      const token = authenticate.getToken({ _id: req.user._id });
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.json({
+        success: true,
+        token: token,
+        status: "You are successfully logged in!",
+      });
+    }
+  }
+);
+
 router.post("/signup", cors.corsWithOptions, (req, res) => {
   User.register(
     new User({ username: req.body.username }),

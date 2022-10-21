b7e0acedb2e2ad4307a86520d73ca25e2a0931b6
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-10-21 14:33:55 -0400

    authentication

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "860227785387295",
+    clientSecret: "868de0a81702a9d4c1c1e7d2a5918d08",
+  },
 };
